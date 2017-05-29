27257ac61e4a28537be933ad1f7f42d9f6b7ffff
Alysson Azevedo <alysson@rxsaude.com.br>
2017-05-29 11:34:51 -0300

    fix bad patterns

--- path/paths.zsh
+++ path/paths.zsh
@@ -1,18 +1,18 @@
 #
 # Paths for all Computers
 #
-if [[-d ~/.composer/vendor/bin]]; then
+if [ -d ~/.composer/vendor/bin ]; then
     # Composer vendor bin path
     export PATH=$PATH:~/.composer/vendor/bin
 fi
 
-if [[-d ~/.config/composer/vendor/bin]]; then
+if [ -d ~/.config/composer/vendor/bin ]; then
     export PATH=$PATH:~/.config/composer/vendor/bin
 fi
 
 # Android SDK Tools
 android_sdks=~/ws/src/android-sdks
-if [[-d "$android_sdks"]]; then
+if [ -d "$android_sdks" ]; then
     export PATH=$PATH:${android_sdks}/tools 
     # Android SDK Platform Tools
     export PATH=$PATH:${android_sdks}/platform-tools
